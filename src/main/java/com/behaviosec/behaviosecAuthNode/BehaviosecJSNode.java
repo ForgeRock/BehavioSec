4803082e4350d5bae6d0063a451a98b076cdc0c6
Northshoots <lr@harmony.ai>
2019-08-14 06:48:51 -0700

    fixed login issiues, and errors in JS

--- src/main/java/com/behaviosec/behaviosecAuthNode/BehaviosecJSNode.java
+++ src/main/java/com/behaviosec/behaviosecAuthNode/BehaviosecJSNode.java
@@ -58,12 +58,12 @@ public class BehaviosecJSNode extends SingleOutcomeNode {
          */
         @Attribute(order = 10)
         default String fileName() {
-            return "behaviosec.js";
+            return "collector.min.js";
         }
 
         @Attribute(order = 20)
         default String scriptResult() {
-            return "behaviosec";
+            return "bdata";
         }
     }
 
@@ -158,42 +158,41 @@ public class BehaviosecJSNode extends SingleOutcomeNode {
 
     public static String createClientSideScriptExecutorFunction(String script, String outputParameterId,
                                                                 boolean clientSideScriptEnabled, String context) {
-        String collectingDataMessage = "";
-        if (clientSideScriptEnabled) {
-            collectingDataMessage = "messenger.messages.addMessage( message );\n";
-        }
-
-        String spinningWheelScript = "if (window.require) {\n" +
-                "    var messenger = require(\"org/forgerock/commons/ui/common/components/Messages\"),\n" +
-                "        spinner =  require(\"org/forgerock/commons/ui/common/main/SpinnerManager\"),\n" +
-                "        message =  {message:\"Collecting Data...\", type:\"info\"};\n" +
-                "    spinner.showSpinner();\n" +
-                collectingDataMessage +
-                "}";
+//        String collectingDataMessage = "";
+//        if (clientSideScriptEnabled) {
+//            collectingDataMessage = "messenger.messages.addMessage( message );\n";
+//        }
+//
+//        String spinningWheelScript = "if (window.require) {\n" +
+//                "    var messenger = require(\"org/forgerock/commons/ui/common/components/Messages\"),\n" +
+//                "        spinner =  require(\"org/forgerock/commons/ui/common/main/SpinnerManager\"),\n" +
+//                "        message =  {message:\"Collecting Data...\", type:\"info\"};\n" +
+//                "    spinner.showSpinner();\n" +
+//                collectingDataMessage +
+//                "}";
 
         return String.format(
 //                spinningWheelScript +
                 "(function(output) {\n" +
-                        "    var autoSubmitDelay = 0,\n" +
-                        "        submitted = false,\n" +
-                        "        context = %s;\n" + //injecting context in form of JSON
-                        "    function submit1() {\n" +
-                        "        console.log(\"submitted = \" + submitted)\n" +
-                        "        if (submitted) {\n" +
-                        "            return;\n" +
-                        "        }" +
-                        "        if (!(typeof $ == 'function')) {\n" + // Crude detection to see if XUI is not present.
-                        "            document.getElementById('loginButton_0').click();\n" +
-                        "        } else {\n" +
-                        "            $('input[type=submit]').click();\n" +
-                        "        }\n" +
-                        "        submitted = true;\n" +
-                        "    }\n" +
+//                        "    var autoSubmitDelay = 0,\n" +
+//                        "        submitted = false,\n" +
+//                        "        context = %s;\n" + //injecting context in form of JSON
+//                        "    function submit1() {\n" +
+//                        "        console.log(\"submitted = \" + submitted)\n" +
+//                        "        if (submitted) {\n" +
+//                        "            return;\n" +
+//                        "        }" +
+//                        "        if (!(typeof $ == 'function')) {\n" + // Crude detection to see if XUI is not present.
+//                        "            document.getElementById('loginButton_0').click();\n" +
+//                        "        } else {\n" +
+//                        "            $('input[type=submit]').click();\n" +
+//                        "        }\n" +
+//                        "        submitted = true;\n" +
+//                        "    }\n" +
                         "    %s\n" + // script
-                        "    setTimeout(submit1, autoSubmitDelay);\n" +
-                        "}) (document.forms[0].elements['%s']);\n", // outputParameterId
-                context,
-                script,
-                outputParameterId);
+//                        "    setTimeout(submit1, autoSubmitDelay);\n" +
+                        "}) (document);\n", // outputParameterId
+                script
+                );
     }
 }
